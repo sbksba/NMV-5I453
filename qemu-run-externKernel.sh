4d61df64c29aec23d73492549a97336da3c4d5c7
sbksba <sbksba@gmail.com>
2015-10-28 15:46:08 +0100

    [ADD] Ajout du lanceur de la vm

---
+++ qemu-run-externKernel.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Fixer les variables avec les chemins de vos fichiers
+HDA=/tmp/nmv-tp.img
+HDB=~/cours/NMV/Images/myHome.img
+KERNEL=/tmp/linux-4.2.3/arch/x86/boot/bzImage
+
+# Si besoin ajouter une option pour le kernel
+CMDLINE='root=/dev/sda1 rw vga=792 console=ttyS0 kgdboc=ttyS1'
+
+exec qemu-system-x86_64 -hda "${HDA}" -hdb "${HDB}" -serial stdio -serial pty -net nic -net user -boot c -m 2G -kernel "${KERNEL}" -append "${CMDLINE}"
