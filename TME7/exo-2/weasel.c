169a35b80d332c910e25ba44b9d41e1e33ff9af8
sbksba <sbksba@gmail.com>
2015-12-16 14:15:35 +0100

    [MOD] Ajout du TME7

--- TME7/exo-2/weasel.c
+++ TME7/exo-2/weasel.c
@@ -11,33 +11,52 @@ MODULE_DESCRIPTION("Module affichant l'adresse et la taille de la dentry");
 MODULE_AUTHOR("Houssem Kanzari & benjamin bielle");
 MODULE_LICENSE("GPL");
 
-static int weasel_open(struct inode *inode, struct file *file);
+const char *msg = "I'm a weasel\n";
 
-static int weasel_read(struct file *file, char __user *user_buf,
-		       size_t length, loff_t *offset);
-
-static const struct file_operations proc_file_ops = {
-	.owner = THIS_MODULE,
-	.open = simple_open,
-	.llseek = noop_lseek,
-	.read = weasel_read,
-	.release = single_release,
-};
-
-/* static int weasel_open(struct inode *inode, struct file *file) */
-/* { */
-/* 	return single_open(file, weasel_proc_show, NULL); */
-/* } */
-
+/* QUESTION 3
 static int weasel_read(struct file *file, char __user *user_buf,
 		       size_t length, loff_t *offset)
 {
 	pr_info("[WEASEL_READ] (/proc/%s) called\n", "weasel");
 
-       	return simple_read_from_buffer(user_buf, length, offset,
-				       "I'm a weasel\n",
-				       sizeof("I'm a weasel\n"));
+       	return simple_read_from_buffer(user_buf, length, offset,msg,
+				       sizeof(msg));
 }
+
+static const struct file_operations proc_file_ops = {
+	.owner = THIS_MODULE,
+	.open = simple_open,
+	.llseek = noop_llseek,
+	.read = weasel_read,
+};
+*/
+
+static int weasel_show(struct seq_file *sf, void *v)
+{
+	struct hlist_node *pos;
+	struct dentry *tpos;
+
+	pr_info("[WEASEL_SHOW]\n");
+	
+	hlist_bl_for_each_entry(tpos, pos,
+				(dentry_hashtable->first), d_hash) {
+		seq_printf(sf, "%s\n", tpos->d_name.name);
+	}
+}
+
+static int weasel_open(struct inode *inode, struct file *file)
+{
+	pr_info("[WEASEL_OPEN]\n");
+	return single_open(file, weasel_show, NULL);
+}
+
+static const struct file_operations proc_file_ops = {
+	.owner = THIS_MODULE,
+	.open = weasel_open,
+	.llseek = seq_lseek,
+	.read = seq_read,
+	.release = single_release,
+};
 
 static int __init weasel_init(void)
 {
@@ -47,9 +66,9 @@ static int __init weasel_init(void)
 	pr_info("[WEASEL INIT]\n");
 
 	size = 1U << d_hash_shift;
-	pr_info("Adresse dentry_hashtable : %pk\n
-Taille dentry_hashtable  : %d\n", dentry_hashtable, size);
-	proc_file_entry = proc_create("weasel", 0, NULL, &proc_file_ops);
+	pr_info("Adresse dentry_hashtable : %pk\n", dentry_hashtable);
+	pr_info("Taille dentry_hashtable  : %d\n", size);
+	proc_file_entry = proc_create("weasel", 0666, NULL, &proc_file_ops);
 
 	if (proc_file_entry == NULL)
 		return -ENOMEM;
