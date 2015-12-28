5bed0d4d95d871fe37caa4a55e14b36d89e42c2d
sbksba <sbksba@gmail.com>
2015-12-28 18:19:01 +0100

    [MOD] Simple correction de la recherche de la table syscall

--- TME8/exo-2/hijack.c
+++ TME8/exo-2/hijack.c
@@ -1,25 +1,19 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/errno.h>
-#include <linux/types.h>
 #include <linux/unistd.h>
-#include <asm/cacheflush.h>
-#include <asm/page.h>
-#include <asm/current.h>
-#include <linux/sched.h>
-#include <linux/kallsyms.h>
 #include <linux/syscalls.h>
 #include <linux/delay.h>
+#include <linux/tty.h>
 
 MODULE_DESCRIPTION("Module Hijack SIG");
 MODULE_AUTHOR("Houssem Kanzari & benjamin bielle");
 MODULE_LICENSE("GPL");
 
-void **syscall_table;
+/* void **syscall_table; */
+void **syscall_table_V2;
 
-/* WORK BUT NO CLEAN
-unsigned long **find_sys_call_table(void)
+unsigned long **find_sys_call_table_V2(void)
 {
 	unsigned long int offset = PAGE_OFFSET;
 	unsigned long **sct;
@@ -33,38 +27,40 @@ unsigned long **find_sys_call_table(void)
 
 	return NULL;
 }
-*/
 
-unsigned long **find_sys_call_table(void)
-{
-	unsigned long ptr;
-	unsigned long *p;
+/* unsigned long **find_sys_call_table(void) */
+/* { */
+/* 	unsigned long **sct; */
+/* 	unsigned long ptr; */
+/* 	unsigned long *p; */
 
-	for (ptr = (unsigned long)sys_close;
-	     ptr < (unsigned long)&loops_per_jiffy;
-	     ptr += sizeof(void *)) {
+/* 	for (ptr = (unsigned long)sys_close; */
+/* 	     ptr < (unsigned long)&loops_per_jiffy; */
+/* 	     ptr += sizeof(void *)) { */
 
-		p = (unsigned long *)ptr;
+/* 		p = (unsigned long *)ptr; */
 
-		if (p[__NR_close] == (unsigned long)sys_close) {
-			pr_info("Found the sys_call_table!!!\n");
-			return (unsigned long **)p;
-		}
-	}
+/* 		if (p[__NR_close] == (unsigned long)sys_close) { */
+/* 			pr_info("Found the sys_call_table!!!\n"); */
+/* 			sct = (unsigned long **)p; */
+/* 			return &sct[0]; */
+/* 		} */
+/* 	} */
 
-	return NULL;
-}
+/* 	return NULL; */
+/* } */
 
 
 static int __init hijack_init(void)
 {
 	pr_info("[HIJACK_INIT]\n");
-	syscall_table = (void **)find_sys_call_table();
+	/* syscall_table = (void **)find_sys_call_table(); */
+	syscall_table_V2 = (void **)find_sys_call_table_V2();
 
-	if (!syscall_table)
+	if (!syscall_table_V2)
 		pr_info("Cannot find the system call address !!!\n");
 	else
-		pr_info("SYS_CALL_TABLE address : 0x%lu\n", (unsigned long)syscall_table);
+		pr_info("SYS_CALL_TABLE address : v2(%p)\n", &syscall_table_V2);
 	
 	return 0;
 }
